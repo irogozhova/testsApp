7af34b28170ed5ed16354f9055f0dbfe31439153
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2020-03-17 22:43:00 +0300

    Added tabs for authorization/registration

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,6 @@
 import React, { PureComponent } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 import styles from './LoginPage.module.scss';
 
@@ -6,18 +8,52 @@ class LoginPage extends PureComponent {
   render() {
     return (
       <div className={styles.root}>
-        <h1>Авторизуйтесь, пожалуйста:</h1>
-        <form>
-          <label>
-            логин:
-            <input type="text" name="username" />
-          </label>
-          <label>
-            пароль:
-            <input type="password" name="password" />
-          </label>
-          <input type="submit" value="Войти" />
-        </form>
+        <Tabs>
+          <TabList>
+            <Tab>Авторизация</Tab>
+            <Tab>Регистрация</Tab>
+          </TabList>
+          <TabPanel>
+            <form>
+              <label>
+                логин:
+                <input type="text" name="username" />
+              </label>
+              <label>
+                пароль:
+                <input type="password" name="password" />
+              </label>
+              <input type="submit" value="Войти" />
+            </form>
+          </TabPanel>
+          <TabPanel>
+            <form>
+              <label>
+                логин:
+                <input type="text" name="username" />
+              </label>
+              <label>
+                пароль:
+                <input type="password" name="password" />
+              </label>
+              <label>
+                подтвердите пароль:
+                <input type="password" name="password" />
+              </label>
+              <label>
+                Админ:
+                <input
+                  name="isAdmin"
+                  type="checkbox"
+                  checked={true}
+                  // checked={this.state.isGoing}
+                  // onChange={this.handleInputChange}
+                />
+              </label>
+              <input type="submit" value="Войти" />
+            </form>
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
